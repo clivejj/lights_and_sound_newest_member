4704a5835b76b14b2e7aa55b7983bb459d03c95b
clivejj <cjohnston1@stuy.edu>
2018-02-07 21:25:31 -0500

    hw

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 var canvas = document.getElementById("cj");
-var clearButton = document.getElementById("clear");
 var stopButton = document.getElementById("stop");
 
 var ctx = canvas.getContext("2d");
 
-clearButton.addEventListener('click', clear);
-stopButton.addEventListener('click', stop);
-
-function clear(e) {
-    console.log("clearing!");
-    ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-};
-
+var requestID = 0;
+var expanding = true;
 
 canvas.addEventListener('click', draw);
-function draw(e) {
-    console.log("drawing!");
-ctx.begin
-    ctx.arc(x, y, 20, 0, 2 * Math.PI);
-    ctx.endPath();
-    ctx.fill();
-    lastX = x;
-    lastY = y;
-};
 
+function draw(e) {
+    window.cancelAnimationFrame(requestID);
+    var r = 1;
+    var augmenter = 1;
+    function circle() {
+	clear();
+	ctx.beginPath();
+	ctx.arc(250, 250, r, 0, 2 * Math.PI);
+	ctx.fill();
+	if (r == 1)
+	    augmenter = 1;
+	if (r == 250)
+	    augmenter = -1;
+	r += augmenter;
+	requestID = window.requestAnimationFrame(circle);
+    }
+    circle();
+}
+
+stopButton.addEventListener('click', stop);
+
+function stop(e) {
+    window.cancelAnimationFrame(requestID);
+}
+
+function clear() {
+    ctx.clearRect(0, 0, 500, 500);
+}
+
+
+    
 
 
 
