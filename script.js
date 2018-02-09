70b9a8fb5b62d5d7c19972b74ccf30076ebb332e
clivejj <cjohnston1@stuy.edu>
2018-02-09 00:26:03 -0500

    hw

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var canvas = document.getElementById("cj");
 var stopButton = document.getElementById("stop");
+var growButton = document.getElementById("grow");
+var bounceButton = document.getElementById("bounce");
 
 var ctx = canvas.getContext("2d");
 
 var requestID = 0;
 var expanding = true;
 
-canvas.addEventListener('click', draw);
+growButton.addEventListener('click', draw);
 
 function draw(e) {
     window.cancelAnimationFrame(requestID);
@@ -33,6 +35,34 @@ function stop(e) {
     window.cancelAnimationFrame(requestID);
 }
 
+bounceButton.addEventListener('click', bounce);
+
+function bounce(e) {
+    window.cancelAnimationFrame(requestID);
+    var dx = 1;
+    var dy = 2;
+    var x = 250;
+    var y = 250;
+    function move() {
+	clear();
+	ctx.beginPath();
+	ctx.arc(x, y, 5, 0, 2 * Math.PI);
+	ctx.fill();
+	if (x == 0 || x == 500) {
+	    dx *= -1;
+	}
+	if (y == 0 || y == 500) {
+	    dy *= -1;
+	}
+	x += dx;
+	y += dy;
+	requestID = window.requestAnimationFrame(move);
+    }
+    move();
+
+}
+	
+    
 function clear() {
     ctx.clearRect(0, 0, 500, 500);
 }
